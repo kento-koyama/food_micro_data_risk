0c0f172968f87e3b0552d454081edb9a7a678f03
Junpei Hosoe <86196108+junpei05@users.noreply.github.com>
2025-09-29 18:52:03 +0900

    Update main_eng.py

--- software/concentration_of_contamination/pages/main_eng.py
+++ software/concentration_of_contamination/pages/main_eng.py
@@ -102,7 +102,7 @@ df['Organism_Detail'] = df['Organism']
 df['Organism'] = df['Organism'].apply(lambda x: 'Campylobacter spp.' if 'Campylobacter' in str(x) else x)
 df['Organism_LaTeX'] = df['Organism'].apply(format_bacteria_name_latex)
 
-df = df.loc[:, ['Year', 'Food Category', 'Food Name', 'Food details', 'Organism', 'Organism_Detail', 'Organism_LaTeX', 'method', 'log CFU/g', 'MPN per g', 'Concentration', 'Unit', 'Agency', 'Survey', 'Source URL', '閲覧日', 'detail information']]
+df = df.loc[:, ['Year', 'Food Category', 'Food Name', 'Food details', 'Organism', 'Organism_Detail', 'Organism_LaTeX', 'method', 'log CFU/g', 'MPN per g', 'Concentration', 'Unit', 'Agency', 'Survey', 'Source URL', 'Access Date', 'detail information']]
 
 # === Filters ===
 cat_opts = ["", "All"] + list(df['Food Category'].dropna().unique())
