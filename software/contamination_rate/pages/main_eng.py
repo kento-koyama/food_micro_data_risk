8b2b3bb99f08623423a7d56c7ac0b5a574e33c40
Junpei Hosoe <86196108+junpei05@users.noreply.github.com>
2025-10-24 13:15:13 +0900

    Update main_eng.py

--- software/contamination_rate/pages/main_eng.py
+++ software/contamination_rate/pages/main_eng.py
@@ -123,7 +123,7 @@ if edible_only:
         df_filtered = df_filtered[~(df_filtered['Food Name'].astype(str).str.contains("内容物", case=False, na=False))]
         df_filtered = df_filtered[~(df_filtered['Food Name'].astype(str).str.contains("Cecal content", case=False, na=False))]
         df_filtered = df_filtered[~(df_filtered['Food Name'].astype(str).str.contains("Fecal sample", case=False, na=False))]
-        df_filtered = df_filtered[~(df_filtered['Food Name'].astype(str).str.contains("Gut content", case=False, na=False))
+        df_filtered = df_filtered[~(df_filtered['Food Name'].astype(str).str.contains("Gut content", case=False, na=False))]
         df_filtered = df_filtered[~(df_filtered['Food Name'].astype(str).str.contains("Cutaneous swabs", case=False, na=False))]
 
 if selected_group == "" and (selected_food != "" or selected_bacteria != "" or selected_institution != ""):
