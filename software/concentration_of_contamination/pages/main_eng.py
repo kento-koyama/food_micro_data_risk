38e5e65e992c9d2b44119a78820d0e0a0a6bdd51
Junpei Hosoe <86196108+junpei05@users.noreply.github.com>
2025-12-01 13:39:04 +0900

    Update main_eng.py

--- software/concentration_of_contamination/pages/main_eng.py
+++ software/concentration_of_contamination/pages/main_eng.py
@@ -282,6 +282,22 @@ language_switch_html = f"""
 """
 st.markdown(language_switch_html, unsafe_allow_html=True)
 
+# Header
+HIDE_MENU_STYLE = """
+<style>
+#MainMenu {
+    visibility: hidden;
+    height: 0%;
+}
+header {
+    visibility: hidden;
+    height: 0%;
+}
+</style>
+"""
+st.markdown(HIDE_MENU_STYLE, unsafe_allow_html=True)
+
+
 # === Footer ===
 st.markdown("""
 <style>
