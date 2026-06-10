6a4b609b96bc3c0b735c7ac7c6478952da85c616
Junpei Hosoe <86196108+junpei05@users.noreply.github.com>
2026-06-10 18:57:44 +0900

    Update main_eng.py

--- software/concentration_of_contamination/pages/main_eng.py
+++ software/concentration_of_contamination/pages/main_eng.py
@@ -493,6 +493,6 @@ st.markdown("""
 }
 </style>
 <div class="footer">
-    For inquiries, click <a href="https://docs.google.com/forms/d/e/1FAIpQLSf2FwkiAWmr3g_50BpPAx5_87w3pwLMPRYeKwCFSfqgSJ1iTA/viewform?usp=header" target="_blank">here</a>.
+    For inquiries or data submission, click <a href="https://docs.google.com/forms/d/e/1FAIpQLSf2FwkiAWmr3g_50BpPAx5_87w3pwLMPRYeKwCFSfqgSJ1iTA/viewform?usp=header" target="_blank">here</a>.
 </div>
 """, unsafe_allow_html=True)
